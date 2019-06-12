352c74ce7c88983e2bf17ad22f25ba86d24fba79
ayyyyy <mcneary.david@gmail.com>
2019-06-12 15:15:02 -0700

    Updates

---
+++ precache-manifest.9dadb4d3504b1b63f058bd5532674a52.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a64ca40dcf74c7816379f34f19dda3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8e18ecbe32efd3243102",
+    "url": "/clicky-game/static/css/main.1c7c25c1.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "8e18ecbe32efd3243102",
+    "url": "/clicky-game/static/js/main.fc514423.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "8e3a8b8b4ae80f0110ca50830ed9910f",
+    "url": "/clicky-game/static/media/anquetil.8e3a8b8b.png"
+  },
+  {
+    "revision": "1eda80d33cce2740da2f578a7f1b5918",
+    "url": "/clicky-game/static/media/armstrong.1eda80d3.jpg"
+  },
+  {
+    "revision": "3b3cd05c4600821e368e9e6193e8ba79",
+    "url": "/clicky-game/static/media/bartali-full.3b3cd05c.jpg"
+  },
+  {
+    "revision": "03c35ee6d94851ac5f38ae2c07b01f09",
+    "url": "/clicky-game/static/media/bartali.03c35ee6.jpg"
+  },
+  {
+    "revision": "e21cd45e8670c06a044d2526d5ce6bb6",
+    "url": "/clicky-game/static/media/contador.e21cd45e.jpg"
+  },
+  {
+    "revision": "4c52b6e476abed96bb46e6ec946db5e5",
+    "url": "/clicky-game/static/media/froome.4c52b6e4.jpg"
+  },
+  {
+    "revision": "d5d88a7252b602ce36bf23c26936985d",
+    "url": "/clicky-game/static/media/hinault.d5d88a72.jpg"
+  },
+  {
+    "revision": "394e18535f162aeed37ca41825746a42",
+    "url": "/clicky-game/static/media/indurain.394e1853.jpg"
+  },
+  {
+    "revision": "c0106821e55d68d3567ec170a26b0b0d",
+    "url": "/clicky-game/static/media/kelly.c0106821.jpg"
+  },
+  {
+    "revision": "e79f5fd7e655b37e124ff1713d28a551",
+    "url": "/clicky-game/static/media/lemond.e79f5fd7.jpg"
+  },
+  {
+    "revision": "855b5a34966a4c65ee56f71b8303a65a",
+    "url": "/clicky-game/static/media/merckx.855b5a34.jpg"
+  },
+  {
+    "revision": "5c2632c5ef82c218f1da0649dd3c888a",
+    "url": "/clicky-game/static/media/pantani.5c2632c5.jpg"
+  },
+  {
+    "revision": "df12a9594dc125f5d95628fcca15a839",
+    "url": "/clicky-game/static/media/sagan.df12a959.jpg"
+  }
+]);
